98ed6b4d2801935a82fa4b41ee604c16666492a9
Khush786Mohammad <khush786moh@gmail.com>
2024-12-14 14:05:59 +0530

    grocery-list

--- Projects/grocery-list/src/App.js
+++ Projects/grocery-list/src/App.js
@@ -13,57 +13,73 @@ const items = [
         id: 1,
         name: "Cooking Oil",
         price: 120,
+        quantity: 0,
     },
     {
         id: 2,
         name: "Rice",
         price: 60,
+        quantity: 0,
+
     },
     {
         id: 3,
         name: "Pulses",
         price: 100,
+        quantity: 0,
+
     },
     {
         id: 4,
         name: "Sugar",
         price: 45,
+        quantity: 0,
+
     },
     {
         id: 5,
         name: "Flour",
         price: 40,
+        quantity: 0,
+
     },
     {
         id: 6,
         name: "Turmeric",
         price: 200,
+        quantity: 0,
+
     },
     {
         id: 7,
         name: "Coffee",
         price: 2500,
+        quantity: 0,
+
     },
     {
         id: 8,
         name: "Pista",
         price: 850,
+        quantity: 0,
+
     },
 ]
 
 export default function App(){
-    let grocery = [];
 
-    function handleGrocery(gro, quant){
-        grocery = [...grocery, items.filter((it) => (it.name === gro))];
-        console.log(grocery);
+    const [groceryItem, setGroceryItems] = useState([]);
+
+    function handleGrocery(newItem){
+        setGroceryItems( newItem.filter((obj) => obj.quantity) );
     }
+
     return (
         <div className="container">
             <h1>Grocery List</h1>
             <OptionList />
             <FormContainer onHandleGrocery={handleGrocery}/>
-            <AddedItems grocery={grocery}/>
+            <AddedItems groceryItem = {groceryItem}/>
         </div>
     )
 }
@@ -91,7 +107,8 @@ function FormContainer({onHandleGrocery}){
             window.alert("select the item with quantity");
             return;
         }
-        onHandleGrocery(groceryItem, itemQuantity);
+        const newItem = items.filter((it) => (it.name === groceryItem) ? it.quantity += itemQuantity : it);
+        onHandleGrocery(newItem);
     }
     return (
         <form className="form-container" onSubmit = { (e) => {e.preventDefault()}}>
@@ -114,31 +131,57 @@ function ListItem({item}){
     );
 }
 
-function AddedItems(){
+function AddedItems({groceryItem}){
+    const len = groceryItem.length;
+    const total = groceryItem.reduce( (accumulator,currValue) => 
+            accumulator + currValue.quantity*currValue.price,0
+    );
     return(
         <table className="table-container">
             <thead>
-                <th>Sr No</th>
-                <th>Item Name</th>
-                <th>Quantity</th>
-            </thead>
-            <tbody>
                 <tr>
-                    <td colSpan="3" className="no-items-list">No items added yet</td>
+                    <th>Sr No</th>
+                    <th>Item Name</th>
+                    <th>Quantity</th>
                 </tr>
-                <tr className="list-items">
-                    <td colSpan="2">Total</td>
-                    <td>00.00</td>
-                </tr>
-            </tbody>
+            </thead>
+            
+                {
+                    len === 0 ?
+                    <tbody>
+                        <tr>
+                            <td colSpan="3" className="no-items-list">No items added yet</td>
+                        </tr> 
+                        <Total>00.00</Total>
+                    </tbody>
+                    : 
+                    <tbody>
+                        {
+                            groceryItem.map( (item , i) => <AddItems item = {item} key={i} index={i}/> )
+                        }
+                        <Total>{total}</Total>
+                    </tbody>
+                    
+                }            
         </table>
     );
 }
 
-function AddItems(){
+function AddItems({item , index}){
     return(
         <tr>
+            <td>{index+1}</td>
+            <td>{item.name}</td>
+            <td className="added-list-items">{item.price}*{item.quantity}</td>
+        </tr>
+    );
+}
 
+function Total({children}){
+    return (
+        <tr className="list-items">
+            <td colSpan="2">Total</td>
+            <td>{children}</td>
         </tr>
     );
 }
