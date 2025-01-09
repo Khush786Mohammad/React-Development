6209c274b669fea11c648491cc0bb14aaf3de0d3
Khush786Mohammad <khush786moh@gmail.com>
2025-01-09 14:13:03 +0530

    todo

--- Projects/todo-list/src/App.js
+++ Projects/todo-list/src/App.js
@@ -2,26 +2,30 @@
 
 // npm install --save @fortawesome/react-fontawesome
 // npm install --save @fortawesome/free-solid-svg-icons
-import {useState, useRef, useEffect} from "react";
+import {useState, useEffect} from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function App(){
-    
     const [taskList, setTaskList] = useState(function(){
         const task = localStorage.getItem('task');
+        console.log("Getting item from local storeage");
         return task === null ? [] : JSON.parse(task);
     });
+
+    const [editTask, setEditTask] = useState("");
+    const [compareTask, setCompareTask] = useState("");
 
     function onHandleDelete(task){
         setTaskList((curr) => curr.filter((tsk)=> tsk !== task));
     }
 
     function onHandleEdit(task){
-
+        setEditTask(task);
+        setCompareTask(task);
     }
-    
+
     useEffect(function(){
         document.title=`React To-Do List`;
         localStorage.setItem('task',JSON.stringify(taskList));
@@ -30,8 +34,9 @@ function App(){
     return (
         <div className="container">
             <Heading />
-            <Form taskList={taskList} setTaskList={setTaskList}/>
+            <Form setTaskList={setTaskList} />
             <Task taskList={taskList} onDeleteTask={onHandleDelete} onEditTask={onHandleEdit}/>
+            <EditTask compareTask={compareTask} setCompareTask={setCompareTask} editTask={editTask} setEditTask={setEditTask} setTaskList={setTaskList}/>
         </div>
     );
 }
@@ -44,7 +49,6 @@ function Heading(){
 
 function Form({setTaskList}){
     const [task, setTask] = useState("");
-    const myRef = useRef(null);
 
     function onHandleAddTask(){
         if(task.length){
@@ -55,9 +59,33 @@ function Form({setTaskList}){
     
     return (
         <div className="form-container">
-            <input type="text" placeholder="What is the task today?" onChange={(e) => setTask(e.target.value)} value={task} ref={myRef}/>
-            <button className="add-btn" onClick={()=>onHandleAddTask()}>Add Task</button>
+            <input type="text" placeholder="What is the task today?" onChange={(e) => setTask(e.target.value)} value={task}/>
+            <Button onHandle={onHandleAddTask}>Add Task</Button>
         </div>
+    );
+}
+
+function EditTask({compareTask, setCompareTask, editTask, setEditTask, setTaskList}){
+    console.log(compareTask, editTask);
+    function onHandleEditTask(){
+        if(editTask.length){
+            setTaskList((taskList) => taskList.map((task) => task === compareTask ? editTask : task));
+            setEditTask((task) => "");
+            setCompareTask((task)=>"");
+        }
+    }
+    return compareTask.length === 0 ? null : 
+    (
+        <div className="form-container">
+            <input type="text" placeholder="What is the task today?" onChange={(e) => setEditTask(e.target.value)}value={editTask}/>
+            <Button onHandle={onHandleEditTask}>Update</Button>
+        </div>
+    );
+}
+
+function Button({onHandle, children}){
+    return (
+        <button className="add-btn" onClick={()=>onHandle()}>{children}</button>
     );
 }
 
@@ -79,8 +107,8 @@ function List({task, onDeleteTask, onEditTask}){
         setIsCompleted((isCompleted) => isCompleted = !isCompleted);
     }
     return (
-        <li className={isCompleted ? `completed tasks` : "tasks"} onClick={onHandleComplete}>
-            <p>{task}</p>
+        <li className={isCompleted ? `completed tasks` : "tasks"} >
+            <p className="tsk" onClick={onHandleComplete}>{task}</p>
             <div className="icons">
                 <FontAwesomeIcon icon={faPenToSquare} className="edit-icon" onClick={()=>onEditTask(task)}/>
                 <FontAwesomeIcon icon={faTrash} className="trash" onClick={()=>onDeleteTask(task)}/>
